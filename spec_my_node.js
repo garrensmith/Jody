caa53fa4321905d22b3c5b66c6cfe19f5fef4765
GarrenSmith <garren.smith@gmail.com>
2010-10-14 20:04:43 +0000

    jslinted

--- spec_my_node.js
+++ spec_my_node.js
@@ -1,52 +1,55 @@
-var assert = require('assert')
-var sys = require('sys')
+var assert = require('assert');
+var sys = require('sys');
 
-if(typeof Object.create !== 'function'){
-  Object.create = function(o){
-    var F = function() {};
+if (typeof Object.create !== 'function') {
+  Object.create = function (o) {
+    var F = function () {};
     F.prototype = o;
     return new F();
   };
 }
 
-var specCase = function(){};
+var specCase = function () {};
 specCase.desc = "";
-specCase.spec = function(){};
+specCase.spec = function () {};
 
 var specs = [];
 
-var describe = exports.describe = function(description){
+var describe = exports.describe = function (description) {
   return describe;
 };
 
-describe.it = function(desc, spec){
-  console.log('adding ' + desc);
+var createSpecCase = function (desc, spec) {
   var new_spec = Object.create(specCase); 
   new_spec.desc = desc;
   new_spec.spec = spec;
-  specs.push(new_spec)
+  
+  return new_spec;
 };
 
-var runTests = function() {
-  
-  for (var i = 0; i < specs.length; i++){
-    var result = "passed";
-    var specCase = specs[i];
+describe.it = function (desc, spec) {
+  var new_spec = createSpecCase(desc, spec);
+  specs.push(new_spec);
+};
 
-    try{
-    specCase.spec();
-    } catch(e)
+var runTests = function () {
+  
+  for (var i = 0; i < specs.length; i++) {
+    var result = "passed", specCase = specs[i];
+
+    try {
+      specCase.spec();
+    } catch (e)
     {
       result = "failed!";
     }
 
     console.log(specCase.desc + '........' + result);
-
-  };
+  }
 };
 
-process.addListener('exit', function() {
+process.addListener('exit', function () {
   console.log('run specs...'); 
   runTests();
-})
+});
 
