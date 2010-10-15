cfe65e8872a6574d69297841935060a9b95d693a
GarrenSmith <garren.smith@gmail.com>
2010-10-15 14:29:37 +0000

    Handle mulitple describes

--- spec/sample_spec.js
+++ spec/sample_spec.js
@@ -1,16 +1,16 @@
-var spec = require('../lib/spec_my_node.js')
+var describe = require('../lib/spec_my_node.js').describe
 var assert = require("assert");
 var inspect = require('sys').inspect;
 console.log("running");
 //console.log(inspect(spec.describe));
 
-spec.describe("Test 1").it("Should pass",function(){
-  assert.equal(2,1);
+describe("Passing Tests").it("Should pass",function(){
+  assert.equal(1,1);
 });
-/*
-spec.describe("Test 2").it("Should fail",function(){
+
+describe("Test 2").it("Should fail",function(){
   assert.equal(1,2);
-});*/
+});
 
 /*var value = 0;
 spec.describe("Before Test").beforeEach (function (){
