5d73aef095a3bf7a8b5e17e13f157389720c236b
GarrenSmith <garren.smith@gmail.com>
2010-11-07 15:25:06 +0000

    added specs for main Spec Objects

---
+++ spec/bdd_spec.js
@@ -0,0 +1,60 @@
+var describe = require('Jody').describe,
+    describe_under_test = require('../lib/Jody').describe,
+    specs = require('../lib/Jody').specs;
+
+var createdSpecCase
+describe("Describe").
+  beforeEach( function () {
+    describe_under_test("example spec");
+    
+    createdSpecCase = specs.pop();
+  }).
+  it("Should set specified description", function () {
+    createdSpecCase.desc.should().beEqual("example spec");
+  }).
+  it("Should create empty array of specs", function () {
+    createdSpecCase.specs.length.should().beEqual(0);
+  });
+
+
+describe("Describe with a beforeEach").
+  it("Should create beforeEachMethod", function () {
+    var beforeEachFunction = function () {};
+    
+    describe_under_test("example spec with beforeEach").
+      beforeEach(beforeEachFunction);
+    
+    var specCase = specs.pop();
+
+    specCase.beforeEachMethodBody.should().beEqual(beforeEachFunction);
+  });
+
+
+var specCase = function () {},
+    spec_under_test,
+    itFunction = function () {};
+describe("Describe with new spec").
+  beforeEach(function () {
+    
+    describe_under_test("example spec with spec").
+      it("example spec", itFunction);
+
+    specCase = specs.pop();
+    spec_under_test = specCase.specs[0];
+  }).
+  it("Should add spec to array of specs", function () {
+    specCase.specs.length.should().beEqual(1);
+  }).
+  it("Should set spec description", function () {
+    
+    spec_under_test.desc.should().beEqual("example spec");
+  }).
+  it("Should set the spec method body", function () {
+    spec_under_test.methodBody.should().beEqual(itFunction);
+  }).
+  it("Should set spec failed until proven passed", function () {
+    spec_under_test.passed.should().beFalse();
+  }).
+  it("Should contain empty error message", function () {
+    spec_under_test.error.should().beEqual("");
+  });
