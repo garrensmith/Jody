16b5cf0507a777e44e66bb757965bcf940e98cce
GarrenSmith <garren.smith@gmail.com>
2011-03-10 16:45:14 +0200

    runner fires events

--- spec/eventing_spec.js
+++ spec/eventing_spec.js
@@ -1,6 +1,7 @@
 var describe = require('Jody').describe,
     Spec = require('../lib/Jody').Spec,
     SpecCase = require('../lib/Jody').SpecCase,
+    SpecRunner = require('../lib/runner').Runner,
     runner = require('../lib/runner').runSpecCase;
 
   describe("Spec Done Event").
@@ -52,6 +53,67 @@ describe("SpecCase Done Event").
 
     runner(specCase);
 
+  });
+
+describe('Runner').
+  it('Should fire on specCase complete', function (async) {
+    var fired = false;
+
+    var specCase = new SpecCase("testing");
+
+    specCase.it("spec1", function () {});
+    specCase.it("spec2", function () {});
+    specCase.it("spec3", function () {});
+
+    var runner = new SpecRunner();
+
+    runner.on('done', async(function() {
+      console.log("boom");
+      fired = false;
+      fired.should().beTrue();
+    }));
+
+    specs = [];
+    specs.push(specCase);
+    runner.run(specs);
+  }).
+  it('Should fire on all specCases complete', function (async) {
+    var fired = false;
+
+    var specCase1 = new SpecCase("testing1");
+
+    specCase1.it("spec1", function () {});
+    specCase1.it("spec2", function () {});
+    specCase1.it("spec3", function () {});
+
+    var specCase2 = new SpecCase("testing2");
+
+    specCase2.it("spec1", function () {});
+    specCase2.it("spec2", function () {});
+    specCase2.it("spec3", function () {});
+
+
+    var specCase3 = new SpecCase("testing3");
+
+    specCase3.it("spec1", function () {});
+    specCase3.it("spec2", function () {});
+    specCase3.it("spec3", function () {});
+
+
+    var runner = new SpecRunner();
+
+    runner.on('done', async(function() {
+      console.log("boom");
+      fired = false;
+      fired.should().beTrue();
+    }));
+
+    specs = [];
+    specs.push(specCase1);
+    specs.push(specCase2);
+    specs.push(specCase3);
+
+    runner.run(specs);
   });
 
 
