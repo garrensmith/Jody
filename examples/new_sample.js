d789526fc7dcd6cee05175335cd1871ac2ab49da
garren smith <garren.smith@gmail.com>
2011-05-17 09:31:31 +0000

    write out new api style

---
+++ examples/new_sample.js
@@ -0,0 +1,46 @@
+var describe = require('Jody').describe;
+
+var value = "";
+
+require.('Jody').beforeAll(function () {
+      value = "set in beforeAll"
+   
+});
+
+describe('New Api').
+    it("Should have a global before all", function () {
+       value.should().beEqual("npmset in beforeAll");
+    }).
+    it("Should make async easy", function () {
+        var counter = 0;
+        
+        setTimeout(function () {
+            counter++;
+            
+            counter.should().beEqual(1);
+            async_complete();
+        }, 10);
+        
+        async_wait();        
+    }).
+    it("Should support multiple callbacks", function () {
+         var counter = 0;
+        
+        setTimeout(function () {
+            counter++;
+            
+            counter.should().beEqual(1);
+            async_complete();
+        },10);
+        
+        setTimeout(function () {
+            counter++;
+            
+            counter.should().beEqual(2);
+            async_complete();
+        },20);
+        
+        async_wait(2);
+    });
+    
+
