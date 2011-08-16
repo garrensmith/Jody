70ebd32d5a389df1e3ee4453fe452984c6e0188d
Garren Smith <garren.smith@gmail.com>
2011-08-16 22:57:33 +0200

    added dsl style

--- examples/embedded_dsl.js
+++ examples/embedded_dsl.js
@@ -1,26 +1,27 @@
 var describe = require('Jody').describe;
 
 
-describe("Embedded API", function () {
+describe("Embedded API", function (spec) {
 
-  it("Should work like this", function () {
+  spec.it("Should work like this", function () {
     var i = 0;
 
     i.should().beEqual(0);
 
   });
 
-  it("Should notice a failure", function () {
+  spec.it("Should notice a failure", function () {
     var i = -1;
 
     i.should().beEqual(0);
 
   });
 
-  it("Should support async operations", function (async) {
+  spec.it("Should support async operations", function (async) {
     var i = 0;
 
     setTimeout(async(function () {
+      i++;
       i.should().beEqual(2);
     }),200);
 
@@ -31,22 +32,6 @@ describe("Embedded API", function () {
 });
 
 
-var Weird = function () {
-
-  hello = function () {
-    console.log("Hello");
-    console.log(this);
-  };
-
-  console.dir(this);
-  
-  hello();
-
-};
-
-//new Weird();
-
-console.dir(it);
 
 
 
